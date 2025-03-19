2e13c24582fb4a6de6f43004c61ed9be09c7f5da
Alex <akruhlik@duckduckgo.com>
2025-03-19 18:57:39 +0100

    Add compacter

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using LevelDB;
+
+var dir = "TODO";
+foreach (var file in Directory.GetFiles(dir))
+{
+    var bytes = File.ReadAllBytes(file);
+    var str = Encoding.ASCII.GetString(bytes);
+    Console.Write(Path.GetRelativePath(dir, file)+"|");
+    Console.WriteLine(str.Contains("foobar") ? "Contains foobar" : "Does not contain foobar");
+}
+
+Console.WriteLine("---------------");
+var options = new Options { CreateIfMissing = false };
+var db = new DB(options, dir);
+foreach (var (key, value) in db)
+{
+    var keyStr = Encoding.ASCII.GetString(key);
+    var valueStr = Encoding.ASCII.GetString(value);
+    Console.WriteLine($"{keyStr} => {valueStr}");
+}
+Console.WriteLine("---------------");
+db.Compact();
+db.Close();
+db.Dispose();
+
+foreach (var file in Directory.GetFiles(dir))
+{
+    var bytes = File.ReadAllBytes(file);
+    var str = Encoding.ASCII.GetString(bytes);
+    Console.Write(Path.GetRelativePath(dir, file)+"|");
+    Console.WriteLine(str.Contains("foobar") ? "Contains foobar" : "Does not contain foobar");
+}
